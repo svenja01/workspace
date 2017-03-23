9669901300669e2b2d358950692d2c23934bf392
Svenja Sutter <ssutter@bljzbw.local>
2017-03-23 09:25:42 +0100

    commit

--- meinPlugin.php
+++ meinPlugin.php
@@ -8,8 +8,9 @@ Author: Svenja Sutter
 add_action('admin_menu', 'myPlugin');
 
 function myPlugin(){
-    add_menu_page('Hilfe1','Hilfe2','edit_posts','meinPluginurl','myPlugin_echo','../wp-content/plugins/myPlugin/myPlugin.png',4);
-    
+    add_menu_page('Hilfe1','comments','edit_posts','meinPluginurl','myPlugin_echo','../wp-content/plugins/myPlugin/myPlugin.png',4);
+
+    //Funktion
     function myPlugin_echo(){
     global $wpdb;
     echo '<h1>Hilfebereich</h1>';
